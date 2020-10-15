ef4358b8e9520d49faf6272c0011dd575bfad7bf
Eugene Moshchin <ztrancer@gmail.com>
2020-10-15 14:46:05 +0300

    Add a Task1

---
+++ academy-new/src/by/academy/homework/Task1.java
@@ -0,0 +1,56 @@
+package by.academy.homework;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+
+//		int discount;
+		float discountSum;
+		float finalPrice;
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input customers age \n");
+		int customerAge = in.nextInt();
+		System.out.println("Input customerSum \n");
+		float customerSum = in.nextFloat();
+//		int customerSum = in.nextInt();
+		in.close();
+
+		if (customerSum < 100) {
+			discountSum = (customerSum * 5) / 100;
+			finalPrice = customerSum - discountSum;
+			System.out.println("Total price is " + finalPrice);
+		} else if (100 <= customerSum && customerSum < 200) {
+			discountSum = (customerSum * 7) / 100;
+			finalPrice = customerSum - discountSum;
+			System.out.println("Total price is " + finalPrice);
+		} else if (200 <= customerSum && customerSum < 300) {
+			if(18 <= customerAge) {
+				discountSum = (customerSum * 16) / 100;
+				finalPrice = customerSum - discountSum;
+				System.out.println("Bingo! You win! Total price is " + finalPrice);
+			} else {
+				discountSum = (customerSum * 9) / 100;
+				finalPrice = customerSum - discountSum;
+				System.out.println("Sorry, your discount is fallen down... Total price is " + finalPrice);
+			}
+//			discountSum = (customerSum * 12) / 100;
+//			finalPrice = customerSum - discountSum;
+//			System.out.println("Total price is " + finalPrice);
+		} else if (300 <= customerSum && customerSum < 400) {
+			discountSum = (customerSum * 15) / 100;
+			finalPrice = customerSum - discountSum;
+			System.out.println("Total price is " + finalPrice);
+		} else if (400 <= customerSum) {
+			discountSum = (customerSum * 20) / 100;
+			finalPrice = customerSum - discountSum;
+			System.out.println("Total price is " + finalPrice);
+			
+		} else {
+			System.out.println("Input the correct number ");
+		}
+
+	}
+}
