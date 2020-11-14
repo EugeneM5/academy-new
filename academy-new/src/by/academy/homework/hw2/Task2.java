e610711323623ac22b0d9c5884530d15edad33c9
Eugene Moshchin <ztrancer@gmail.com>
2020-11-14 11:31:04 +0300

    Formatted

--- academy-new/src/by/academy/homework/hw2/Task2.java
+++ academy-new/src/by/academy/homework/hw2/Task2.java
@@ -9,25 +9,24 @@ public class Task2 {
 
 	public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Введите слова через пробел ");
 
-		String text = new String(in.nextLine());
-		String[] word = text.split(" ");
-		in.close();
+		String[] words = (scanner.nextLine().split(" "));
+		scanner.close();
 
 		int wordCount = 0;
 		int n = 0;
 		int repeat = 0;
 		int count0 = 0;
-		String temp = word[0];
+		String temp = words[0];
 
-		for (String i : word) {
+		for (String i : words) {
 			System.out.println(i);
 			wordCount++;
 		}
 		while (n < wordCount) {
-			char[] ch = word[n].toCharArray();
+			char[] ch = words[n].toCharArray();
 			for (int i = 0; i < ch.length; i++) {
 				for (int j = i + 1; j < ch.length; j++) {
 					if (ch[i] == ch[j]) {
@@ -36,7 +35,7 @@ public class Task2 {
 				}
 				if (repeat > count0) {
 					count0 = repeat;
-					temp = word[n];
+					temp = words[n];
 				} else {
 					repeat = 0;
 				}
