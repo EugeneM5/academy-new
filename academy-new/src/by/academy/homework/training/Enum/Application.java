1636e46909b61b3528683c99fccfbaa863528468
Eugene Moshchin <ztrancer@gmail.com>
2020-11-24 19:01:39 +0300

    enums

--- academy-new/src/by/academy/homework/training/Enum/Application.java
+++ academy-new/src/by/academy/homework/training/Enum/Application.java
@@ -1,23 +1,42 @@
 package by.academy.homework.training.Enum;
 
 public class Application {
-
+//Переопределить метод getDescription - для константы Лето метод должен возвращать “Теплое время года”.
 	public static void main(String[] args) {
-
-		Seasons myFavouriteSeason = Seasons.SUMMER;
-//		System.out.println(Seasons.SPRING.getTitle());
-//		
-//		System.out.println(myFavouriteSeason.getTitle());
-		myFavouriteSeason.getDescription();
-		myFavouriteSeason.getDescription(Seasons.WINTER);
 
 		Seasons[] some = Seasons.values();// создан массив КОНСТАНТ и с помощью цикла выведен на печать.
 		for (Seasons s : some) {
+			if(s == Seasons.SUMMER) {
+				System.out.println("ТЁПЛЕНЬКА");
+			}
 			System.out.println(s.ordinal());
 			System.out.println(s);
-			System.out.println(s.getTitle() + " " + s.getTemp());
-
+			System.out.println(s.getDiscription() + " " + s.getTemp());
+			System.out.println("-------------");
 		}
+			System.out.println("----------------------------------------");
+			
+			Seasons myFavouriteSeason = Seasons.SUMMER;
+			
+			switch (myFavouriteSeason) {
+			case AUTUMN:
+				System.out.println("ДОЖДЖ");
+				break;
+			case WINTER:
+				System.out.println("СНЕГГ");
+				break;
+			case SUMMER:
+				System.out.println("ТЯПЛО");
+				break;
+			case SPRING:
+				System.out.println("ТЯПЛО И ДОЖДЖ");
+				break;
+			default:
+				System.out.println("GGRR");
+				break;
+			}
+
+		
 	}
 
 }
--- academy-new/src/by/academy/homework/training/Enum/Seasons.java
+++ academy-new/src/by/academy/homework/training/Enum/Seasons.java
@@ -1,62 +1,25 @@
 package by.academy.homework.training.Enum;
 
 public enum Seasons {
-	WINTER("Зима", -5), SPRING("Весна", 15), SUMMER("Лето", 25), AUTUMN("Осень", 10);
+	WINTER("Холодное время года", -5), SPRING("Теплое время года", 15), SUMMER("Теплое время года", 25),
+	AUTUMN("Холодное время года", 10);
 
-	private String title;
+	private String discription;
 	private int temp;
 
-	private Seasons(String title, int temp) {
-		this.title = title;
+	private Seasons(String discription, int temp) {
+		this.discription = discription;
 		this.temp = temp;
 	}
 
-	private Seasons(String title) {
-		this.title = title;
+	public String getDiscription() {
+		return discription;
 	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
+	
 	public int getTemp() {
 		return temp;
 	}
 
-	public void setTemp(int temp) {
-		this.temp = temp;
-	}
 
-	public void choise(Seasons myFavouriteSeason) {
-		
-		switch (myFavouriteSeason) {
-		case AUTUMN:
-			System.out.println("ДОЖДЖ");
-			break;
-		case WINTER:
-			System.out.println("СНЕГГ");
-			break;
-		case SUMMER:
-			System.out.println("ТЯПЛО");
-			break;
-		case SPRING:
-			System.out.println("ТЯПЛО И ДОЖДЖ");
-			break;
-		default:
-			System.out.println("GGRR");
-			break;
-		}
-	}
-	public void getDescription() {
-		System.out.println("Холодное время года");
-	}
-	
-	public void getDescription(Seasons SUMMER) {// как переопределить на SUMMER
-		System.out.println("Теплое время года");
-	}
-	
+
 }
